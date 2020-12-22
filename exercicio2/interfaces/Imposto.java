58776e40f65333ac56faf181eccc0e0dfdfa29d5
eort <eduardo.santana@gft.com>
2020-12-22 15:27:22 -0300

    Refatoração de todo o projeto

--- exercicio2/interfaces/Imposto.java
+++
@@ -1,6 +0,0 @@
-package poo_exercicios.exercicio2.interfaces;
-
-public interface Imposto {
-
-	public double calculaImposto();
-}
--- exercicio7/Gerente.java
+++
@@ -1,9 +0,0 @@
-package poo_exercicios.exercicio7;
-
-public class Gerente extends Funcionario{
-
-	@Override
-	public double bonificacao() {
-		return super.getSalario() + 10000.00;
-	}
-}
